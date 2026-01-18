2c65c206bb18cb12dcdb8527efdac3596e86a93a
Axel Ståhlbom <cass981028@hotmail.com>
2026-01-18 11:42:25 +0100

    Remove include of cstdint, which couldn't be found by the compiler

--- keyboards/crkbd/keymaps/AxelStahlbom/keymap.c
+++ keyboards/crkbd/keymaps/AxelStahlbom/keymap.c
@@ -16,7 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <cstdint>
 #include QMK_KEYBOARD_H
 
 #include "action_layer.h"
